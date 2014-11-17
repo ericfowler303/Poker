63e0fb7a59757d7138bed0f00b39eecc7df339d2
Eric Fowler <ericfowler303@gmail.com>
2014-11-17 16:44:32 -0700

    Add the Deck class
    uses in list shuffling of elements instead of creating a new list just
    to shuffle

--- Poker/Poker/Program.cs
+++ Poker/Poker/Program.cs
@@ -12,6 +12,72 @@ namespace Poker
         {
         }
     }
+
+    class Deck
+    {
+        private List<Card> unusedCards = new List<Card>();
+        private List<Card> dealtCards = new List<Card>();
+        private Random rng = new Random();
+
+        /// <summary>
+        /// Constructor that initializes the deck of cards
+        /// </summary>
+        public Deck()
+        {
+            // Fill the unusedCards with a set of cards
+
+            // Suits first
+            for (int i = 0; i < 4; i++)
+            {
+                // Cards, which start with 2 and go till Ace (14)
+                for (int j = 2; j < 15; j++)
+                {
+                    // Add this new card to the deck
+                    unusedCards.Add(new Card(i, j));
+                }
+            }
+        }
+
+        public Card DealACard()
+        {
+            // Get a random card from the unused deck
+            int randomIndex = rng.Next(GetNumOfCards());
+            Card tempCard = unusedCards.ElementAt(randomIndex);
+            //Put the card in the dealt deck
+            dealtCards.Add(tempCard);
+            // Remove card from the unused deck
+            unusedCards.RemoveAt(randomIndex);
+
+            // Return that card to the player/hand
+            return tempCard;
+        }
+        public void Shuffle()
+        {
+            // When there is more then 1 card to shuffle
+            if (unusedCards.Count > 1)
+            {
+                // Go through each index of the list
+                for (int i = unusedCards.Count - 1; i >= 0; i--)
+                {
+                    // Pick a random card to swap with this index
+                    Card tmp = unusedCards[i];
+                    int randomIndex = rng.Next(i + 1);
+
+                    //Swap elements
+                    unusedCards[i] = unusedCards[randomIndex];
+                    unusedCards[randomIndex] = tmp;
+                }
+            }
+        }
+        /// <summary>
+        /// Returns the number of unused cards in the deck
+        /// </summary>
+        /// <returns>number of unused cards in the deck</returns>
+        public int GetNumOfCards()
+        {
+            return unusedCards.Count();
+        }
+    }
     class Card
     {
         private enum CardRank
@@ -37,7 +103,7 @@ namespace Poker
         public CardRank Rank { get; set; }
         public CardSuit Suit { get; set; }
         /// <summary>
-        /// Creates a card with a certain Rank and Suit
+        /// Constructor creates a card with a certain Rank and Suit
         /// </summary>
         /// <param name="myRank">Rank based on enum value</param>
         /// <param name="mySuit">Suit based on enum value</param>
